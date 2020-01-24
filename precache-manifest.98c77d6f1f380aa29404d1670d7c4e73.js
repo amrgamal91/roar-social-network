e4ad493c945f329d19b4a39891460ee08e758ee7
Amr Gamal <ag.amrgamal91@gmail.com>
2020-01-25 01:28:13 +0300

    Updates

---
+++ precache-manifest.98c77d6f1f380aa29404d1670d7c4e73.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b451b9b86e5ee18d40a82c2fa4e48eba",
+    "url": "/roar-social-network/index.html"
+  },
+  {
+    "revision": "841127773767f60d701c",
+    "url": "/roar-social-network/static/css/main.4737ee2a.chunk.css"
+  },
+  {
+    "revision": "d48f768601f50add22b5",
+    "url": "/roar-social-network/static/js/2.57027b51.chunk.js"
+  },
+  {
+    "revision": "394e4e14b3f77dd9ae097c24d77b6329",
+    "url": "/roar-social-network/static/js/2.57027b51.chunk.js.LICENSE"
+  },
+  {
+    "revision": "841127773767f60d701c",
+    "url": "/roar-social-network/static/js/main.18e1043a.chunk.js"
+  },
+  {
+    "revision": "6c4ec91a1769b1636fd4",
+    "url": "/roar-social-network/static/js/runtime-main.13955634.js"
+  },
+  {
+    "revision": "06abb69158eb758e4c5ca5516b49ed6f",
+    "url": "/roar-social-network/static/media/brandLogo.06abb691.png"
+  },
+  {
+    "revision": "6732bd426c60b47172b1c7296e886a60",
+    "url": "/roar-social-network/static/media/no-img.6732bd42.png"
+  }
+]);
